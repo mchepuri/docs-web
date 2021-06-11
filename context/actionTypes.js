ac9f3bc02fcf2ec571bc3b039a455f779643f4dd
Murali Chepuri <muralimanohar.chepuri.contractor@move.com>
2021-06-11 15:23:30 +0530

    Refactored to src/app

--- context/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const actionTypes = {
-    setData: 'COMMUNITY_DETAIL/setData',
-    setDataSuccess: 'COMMUNITY_DETAIL/setDataSuccess',
-    setDataFailure: 'COMMUNITY_DETAIL/setDataFailure',
-  };
-  
-  export default actionTypes;
-  
--- context/actions.js
+++
@@ -1,6 +0,0 @@
-import actionTypes from './actionTypes';
-
-export const setData = async (dispatch, data) => {
-  console.log('SetData Called ',data);
-  dispatch({ type: actionTypes.setDataSuccess, data });
-};
