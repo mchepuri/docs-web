678b24f2fdb04d5a915f132db2f4cd17113cb5cb
stej <sai.tej.contractor@move.com>
2021-06-30 16:26:14 +0530

    [UPDATE] updated button color

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,22 +1,30 @@
-import grey from '@material-ui/core/colors/grey';
-import pink from '@material-ui/core/colors/pink';
-import indigo from '@material-ui/core/colors/indigo'
-import { createMuiTheme } from '@material-ui/core/styles';
+import grey from "@material-ui/core/colors/grey";
+import pink from "@material-ui/core/colors/pink";
+import indigo from "@material-ui/core/colors/indigo";
+import { createMuiTheme } from "@material-ui/core/styles";
 const primary = grey[50]; // #f44336
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: primary
+      main: primary,
     },
     secondary: pink,
   },
   overrides: {
     MuiButton: {
-      label: {
-        color: "#191919",
+      root: {
+        background: "linear-gradient(45deg, #485cc6 30%, #485cc6 90%)",
       },
+      label: {
+        color: "white",
+      }
+    },
+    MuiMobileStepper: {
+        dotActive:{
+            background: "linear-gradient(45deg, #485cc6 30%, #485cc6 90%)",
+        }
     }
-  }
+  },
 });
 /* const theme = createMuiTheme({
   palette: {
